390790072d76f14500942f7fc3f2b8f0219dc27e
enzomtp <enzojore@live.fr>
2024-10-18 16:49:05 +0200

    Renamed hasFormation into Offers

--- parcoursup/admin.py
+++ parcoursup/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Establishment
 from .models import Formation
 from .models import Application
-from .models import hasFormation
+from .models import Offers
 
 admin.site.register(Establishment)
 admin.site.register(Formation)
 admin.site.register(Application)
-admin.site.register(hasFormation)
+admin.site.register(Offers)
 
 # Register your models here.
--- parcoursup/models.py
+++ parcoursup/models.py
@@ -6,7 +6,7 @@ class Establishment(models.Model):
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
-    formations = models.ManyToManyField('Formation', through='hasFormation')
+    formations = models.ManyToManyField('Formation', through='Offers')
 
 class Formation(models.Model):
     name = models.CharField(max_length=255)
@@ -18,6 +18,6 @@ class Application(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=255)
 
-class hasFormation(models.Model):
+class Offers(models.Model):
     establishment = models.ForeignKey(Establishment, on_delete=models.CASCADE)
     formation = models.ForeignKey(Formation, on_delete=models.CASCADE)
