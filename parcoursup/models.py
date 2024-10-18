7effc7ea63a683d4188a5ff07de3ae25827c5333
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2024-10-18 15:43:27 +0200

    models done

--- parcoursup/models.py
+++ parcoursup/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Establishment(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    formations = models.ManyToManyField('Formation', through='hasFormation')
+
+class Formation(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    applications = models.ManyToManyField(User, through='Application')
+
+class Application(models.Model):
+    formation = models.ForeignKey(Formation, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=255)
+
+class hasFormation(models.Model):
+    establishment = models.ForeignKey(Establishment, on_delete=models.CASCADE)
+    formation = models.ForeignKey(Formation, on_delete=models.CASCADE)
--- parcoursup/views.py
+++ parcoursup/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'parcoursup/index.twig')
+
 # Create your views here.
