17cdd303487abc0104d25a5e0664f09969dfc880
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2024-10-18 15:49:11 +0200

    admin has access to models

--- parcoursup/admin.py
+++ parcoursup/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Establishment
+from .models import Formation
+from .models import Application
+from .models import hasFormation
+
+admin.site.register(Establishment)
+admin.site.register(Formation)
+admin.site.register(Application)
+admin.site.register(hasFormation)
 
 # Register your models here.
