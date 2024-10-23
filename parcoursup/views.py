6e0d8788d933d7e3064135a2cf0f6ed98a3c6061
enzomtp <enzojore@live.fr>
2024-10-23 09:56:14 +0200

    Fixed /index not showing

--- parcoursup/views.py
+++ parcoursup/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'parcoursup/templates/index.html.twig')
+    return render(request, 'index.html.twig')
 
 # Create your views here.
