54304afe81b365c26ee6e603e1bcd273f90a628a
enzomtp <enzojore@live.fr>
2024-10-23 12:04:51 +0200

    moved tree

--- parcoursup/views.py
+++ parcoursup/views.py
@@ -8,10 +8,10 @@ def index(request):
     return render(request, 'pages/index.html.twig')
 
 def eleveaccueil(request):
-    return render(request, 'pages/eleveaccueil.html.twig', {'Formations': Formations.objects.all()})
+    return render(request, 'accounts/eleveaccueil.html.twig', {'Formations': Formations.objects.all()})
 
 def candidater(request):
-    return render(request, 'pages/candidater.html.twig')
+    return render(request, 'accounts/candidater.html.twig')
 
 def login(request):
     return render(request, 'pages/login.html.twig')
