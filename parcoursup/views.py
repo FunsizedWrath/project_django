ea1920d5f80271419ec8eafcf46d3c4eead7b768
enzomtp <enzojore@live.fr>
2024-10-23 11:00:54 +0200

    pages

--- parcoursup/views.py
+++ parcoursup/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
 def indexe(request):
-    return render(request, 'indexe.html.twig')
+    return render(request, 'pages/indexe.html.twig')
 
 def index(request):
-    return render(request, 'index.html.twig')
+    return render(request, 'pages/index.html.twig')
 
 def eleveaccueil(request):
-    return render(request, 'eleveaccueil.html.twig')
+    return render(request, 'pages/eleveaccueil.html.twig')
 
 # Create your views here.
